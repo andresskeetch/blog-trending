be40c94e144399fe0e05990ee13fd9b5df96d9ed
andresgoh <andreseugenio.gomez@globant.com>
2021-06-12 12:47:02 -0500

    Add hero to home page

--- src/domains/home/containers/components/hero/index.js
+++ src/domains/home/containers/components/hero/index.js
@@ -1,5 +1,33 @@
 import React from "react";
+import Card from "./card";
+import background from "assets/img/pope-francis.png";
+import backgroundBigger from "assets/img/pope-francis.@2x.png";
+import "./styles.scss";
 
 export const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <header class="hero">
+      <img
+        class="hero__background"
+        srcset={`${background} 750w, ${backgroundBigger} 1440w`}
+        sizes="(min-width: 750px) 1440px, 100vw"
+        src={background}
+        alt="Pope Francis"
+      />
+      <div class="max-centered">
+        <div class="hero__featured-card">
+          <Card />
+        </div>
+      </div>
+      <div class="hero__closing-gauge">
+        <div class="closing-gauge__left">
+          <span class="closing-gauge__title">closing in</span>
+        </div>
+        <div class="closing-gauge__right">
+          <span class="closing-gauge__number">22</span>
+          <span class="closing-gauge__desc">days</span>
+        </div>
+      </div>
+    </header>
+  );
 };
