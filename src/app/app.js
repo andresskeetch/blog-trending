fc8f927c64818f60ebdfe790d792cbf44329549c
andresgoh <andreseugenio.gomez@globant.com>
2021-06-12 11:32:21 -0500

    Create project and standard folders

---
+++ src/app/app.js
@@ -0,0 +1,14 @@
+import { Nav } from "components";
+import { Home } from "domains/home";
+import "./styles.scss";
+
+const App = () => {
+  return (
+    <>
+      <Nav />
+      <Home />
+    </>
+  );
+};
+
+export default App;
